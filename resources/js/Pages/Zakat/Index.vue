3329bac101a56fb85a5242add22827234535f24d
yud1 <yud1@csui04.net>
2021-12-24 23:51:51 +0700

    add create new button on Zakat index page

--- resources/js/Pages/Zakat/Index.vue
+++ resources/js/Pages/Zakat/Index.vue
@@ -5,40 +5,49 @@
       <h1 class="text-xl font-semibold leading-tight text-gray-800">
         Transaksi Zakat
       </h1>
-      <table>
-        <thead class="font-bold bg-gray-300 border-b-2">
-          <td class="px-4 py-2">No. Zakat</td>
-          <td class="px-4 py-2">Tanggal</td>
-          <td class="px-4 py-2">Terima dari</td>
-          <td class="px-4 py-2">Petugas</td>
-          <td class="px-4 py-2">Periode</td>
-          <td class="px-4 py-2">Kepala Keluarga</td>
-          <td class="px-4 py-2">Jumlah</td>
-          <td class="px-4 py-2">Action</td>
-        </thead>
-        <tbody>
-          <tr v-for="zakat in zakats.data" :key="zakat.id">
-            <td class="px-4 py-2">{{ zakat.transaction_no }}</td>
-            <td class="px-4 py-2">{{ zakat.transaction_date }}</td>
-            <td class="px-4 py-2">{{ zakat.receive_from_name }}</td>
-            <td class="px-4 py-2">{{ zakat.zakat_pic_name }}</td>
-            <td class="px-4 py-2">{{ zakat.hijri_year }}</td>
-            <td class="px-4 py-2">{{ zakat.family_head }}</td>
-            <td class="px-4 py-2">{{ zakat.total_rp.toLocaleString() }}</td>
-            <td class="px-4 py-2 font-extrabold">
-              <Link
-                :href="route('zakat.edit', zakat.id)"
-                class="text-green-700"
-              >
-                Ubah
-              </Link>
-              <Link @click="destroy(zakat.id)" class="text-red-700">
-                Hapus
-              </Link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="py-6 bg-white border-b border-gray-200">
+        <div class="mb-4">
+          <Link
+            class="px-6 py-2 mb-2 text-green-100 bg-green-500 rounded"
+            :href="route('zakat.create')"
+            >Buat Transaksi</Link
+          >
+        </div>
+        <table>
+          <thead class="font-bold bg-gray-300 border-b-2">
+            <td class="px-4 py-2">No. Zakat</td>
+            <td class="px-4 py-2">Tanggal</td>
+            <td class="px-4 py-2">Terima dari</td>
+            <td class="px-4 py-2">Petugas</td>
+            <td class="px-4 py-2">Periode</td>
+            <td class="px-4 py-2">Kepala Keluarga</td>
+            <td class="px-4 py-2">Jumlah</td>
+            <td class="px-4 py-2">Action</td>
+          </thead>
+          <tbody>
+            <tr v-for="zakat in zakats.data" :key="zakat.id">
+              <td class="px-4 py-2">{{ zakat.transaction_no }}</td>
+              <td class="px-4 py-2">{{ zakat.transaction_date }}</td>
+              <td class="px-4 py-2">{{ zakat.receive_from_name }}</td>
+              <td class="px-4 py-2">{{ zakat.zakat_pic_name }}</td>
+              <td class="px-4 py-2">{{ zakat.hijri_year }}</td>
+              <td class="px-4 py-2">{{ zakat.family_head }}</td>
+              <td class="px-4 py-2">{{ zakat.total_rp.toLocaleString() }}</td>
+              <td class="px-4 py-2 font-extrabold">
+                <Link
+                  :href="route('zakat.edit', zakat.id)"
+                  class="text-green-700"
+                >
+                  Ubah
+                </Link>
+                <Link @click="destroy(zakat.id)" class="text-red-700">
+                  Hapus
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       <pagination :links="zakats.links" />
     </template>
   </BreezeAuthenticatedLayout>
