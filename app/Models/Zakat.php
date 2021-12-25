c8ca3d38dc59efbfc859a1069af81da771b8f30c
yud1 <yud1@csui04.net>
2021-12-25 15:49:53 +0700

    modify Zakat and User model relationships

--- app/Models/Zakat.php
+++ app/Models/Zakat.php
@@ -9,14 +9,20 @@ class Zakat extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'transaction_no', 'receive_from', 'zakat_pic',
+        'transaction_date', 'hijri_year', 'family_head',
+        'total_rp'
+    ];
+
     public function receiveFrom()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'receive_from', 'id');
     }
 
     public function zakatPIC()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'zakat_pic', 'id');
     }
     public function zakatLines()
     {
