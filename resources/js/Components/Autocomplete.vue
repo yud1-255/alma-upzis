8487cefe293bac6c0dbf65e507a2c750d9e596a3
yud1 <yud1@csui04.net>
2022-02-01 18:58:33 +0700

    initial implementation for autocomplete component

---
+++ resources/js/Components/Autocomplete.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="autocomplete">
+    <Input v-model="search" @input="onChange"></Input>
+  </div>
+  <ul v-show="isOpen" class="autocomplete">
+    <li v-if="isLoading" class="loading">loading ...</li>
+    <li
+      v-else
+      v-for="result in results"
+      :key="result[key]"
+      @click="setResult(result)"
+      class="hover:text-gray-700 hover:border-gray-300 hover:bg-gray-100"
+    >
+      {{ result[value] }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import Input from "@/Components/Input.vue";
+
+export default {
+  name: "Autocomplete",
+  props: {
+    items: Array,
+    key: "",
+    value: "",
+    isLoading: false,
+    isAsync: false,
+  },
+  watch: {
+    items: function (value, oldValue) {
+      if (this.isAsync) {
+        this.results = value;
+        this.isOpen = true;
+        this.isLoading = false;
+      }
+    },
+  },
+  components: {
+    Input,
+  },
+  mounted() {
+    document.addEventListener("click", this.handleClickOutside);
+  },
+  destroyed() {
+    document.removeEventListener("click", this.handleClickOutside);
+  },
+  data() {
+    return {
+      search: "",
+      results: [],
+      isOpen: false,
+    };
+  },
+  emits: {
+    input: null,
+    selected: null,
+  },
+  methods: {
+    setResult(result) {
+      this.$emit("selected", result);
+
+      this.search = result[this.value];
+      this.isOpen = false;
+    },
+    filterResults() {
+      this.results = this.items.filter(
+        (item) =>
+          item[this.value].toLowerCase().indexOf(this.search.toLowerCase()) > -1
+      );
+    },
+    onChange() {
+      this.$emit("input", this.search);
+      if (this.isAsync) {
+        this.isLoading = true;
+      } else {
+        this.filterResults();
+        this.isOpen = true;
+      }
+    },
+  },
+  setup() {},
+};
+</script>
+<style>
+.autocomplete-result.is-active,
+.autocomplete-result:hover {
+  background-color: #4aae9b;
+  color: white;
+}
+</style>
