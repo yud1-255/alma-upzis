804900bdfaee5a2b2456e4b7abe426873e4f19e9
yud1 <yud1@csui04.net>
2022-02-06 22:01:47 +0700

    add front end authorization for reports menu

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -45,7 +45,10 @@
                 <div
                   class="cursor-pointer inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
                 >
-                  <BreezeDropdown align="left">
+                  <BreezeDropdown
+                    align="left"
+                    v-if="isAdministrator || isUpzis"
+                  >
                     <template #trigger>
                       <button class="inline-flex">
                         Laporan
@@ -63,9 +66,7 @@
                         </svg></button
                     ></template>
                     <template #content>
-                      <BreezeDropdownLink
-                        :href="route('zakat.muzakkiRecap')"
-                        v-if="isAdministrator || isUpzis"
+                      <BreezeDropdownLink :href="route('zakat.muzakkiRecap')"
                         >Rekap berdasarkan muzakki</BreezeDropdownLink
                       ><BreezeDropdownLink :href="route('zakat.muzakkiList')"
                         >Daftar muzakki</BreezeDropdownLink
