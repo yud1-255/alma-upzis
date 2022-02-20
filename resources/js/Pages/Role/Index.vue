be96743e5c8ce3b713ef90ff424d25c109e3b0b6
yud1 <yud1@csui04.net>
2022-02-21 00:33:24 +0700

    set default select option styling

--- resources/js/Pages/Role/Index.vue
+++ resources/js/Pages/Role/Index.vue
@@ -9,7 +9,7 @@
         <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
           <div class="p-6 bg-white border-b border-gray-200">
             <table class="w-4/5">
-              <thead class="font-bold bg-gray-300 border-b-2">
+              <thead class="font-bold border-b-2">
                 <td class="px-4 py-2">ID</td>
                 <td class="px-4 py-2">Nama</td>
                 <td class="px-4 py-2">Email</td>
