134856445a14a5c97dacb2beb0f0e03182d992ab
yud1 <yud1@csui04.net>
2022-02-21 00:24:44 +0700

    refactored dropdown navigation icon

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -52,19 +52,10 @@
                     <template #trigger>
                       <button class="inline-flex">
                         Laporan
-                        <svg
-                          class="ml-2 -mr-0.5 h-4 w-4"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                          />
-                        </svg></button
-                    ></template>
+
+                        <icon-dropdown />
+                      </button>
+                    </template>
                     <template #content>
                       <BreezeDropdownLink :href="route('zakat.muzakkiRecap')"
                         >Rekap berdasarkan muzakki</BreezeDropdownLink
@@ -91,18 +82,7 @@
                       >
                         {{ $page.props.auth.user.name }}
 
-                        <svg
-                          class="ml-2 -mr-0.5 h-4 w-4"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
+                        <icon-dropdown />
                       </button>
                     </span>
                   </template>
@@ -233,6 +213,8 @@ import BreezeDropdown from "@/Components/Dropdown.vue";
 import BreezeDropdownLink from "@/Components/DropdownLink.vue";
 import BreezeNavLink from "@/Components/NavLink.vue";
 import BreezeResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import IconDropdown from "@/Components/IconDropdown.vue";
+
 import { Link } from "@inertiajs/inertia-vue3";
 
 export default {
@@ -243,6 +225,7 @@ export default {
     BreezeNavLink,
     BreezeResponsiveNavLink,
     Link,
+    IconDropdown,
   },
 
   data() {
