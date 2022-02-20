a236eeb1b7aad7184ad400963b49d986ef67ac9d
yud1 <yud1@csui04.net>
2022-02-20 17:47:08 +0700

    implement styling on muzakki list report

--- resources/js/Pages/Zakat/MuzakkiList.vue
+++ resources/js/Pages/Zakat/MuzakkiList.vue
@@ -3,41 +3,47 @@
   <BreezeAuthenticatedLayout>
     <template #header>
       <h1>Daftar Muzakki</h1>
-      <div class="py-6 bg-white border-b border-gray-200">
-        <table class="w-full">
-          <thead class="font-bold bg-gray-300 border-b-2">
-            <tr>
-              <td rowspan="2" class="px-4 py-2">Kepala Keluarga</td>
-              <td rowspan="2" class="px-4 py-2">Alamat</td>
-              <td rowspan="2" class="px-4 py-2">Kontak</td>
-              <td rowspan="2" class="px-4 py-2">Muzakki</td>
-              <td colspan="2" class="px-4 py-2">Akun Pengguna</td>
-            </tr>
-            <tr>
-              <td class="px-4 py-2">Email</td>
-              <td class="px-4 py-2">Nama</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="family in families.data" :key="family.id">
-              <td class="align-top">{{ family.head_of_family }}</td>
-              <td class="align-top">{{ family.address }}</td>
-              <td class="align-top">{{ family.phone }}</td>
-              <td class="align-top">
-                <ul>
-                  <li v-for="muzakki in family.muzakkis" :key="muzakki.id">
-                    {{ muzakki.name }}
-                  </li>
-                </ul>
-              </td>
-              <td class="align-top">{{ family.user?.email }}</td>
-              <td class="align-top">{{ family.user?.name }}</td>
-            </tr>
-          </tbody>
-        </table>
-        <pagination :links="families.links" />
-      </div>
     </template>
+    <div class="py-6">
+      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+          <div class="p-6 bg-white border-b border-gray-200">
+            <table class="w-full">
+              <thead class="font-bold bg-gray-300 border-b-2">
+                <tr>
+                  <td rowspan="2" class="px-4 py-2">Kepala Keluarga</td>
+                  <td rowspan="2" class="px-4 py-2">Alamat</td>
+                  <td rowspan="2" class="px-4 py-2">Kontak</td>
+                  <td rowspan="2" class="px-4 py-2">Muzakki</td>
+                  <td colspan="2" class="px-4 py-2">Akun Pengguna</td>
+                </tr>
+                <tr>
+                  <td>Email</td>
+                  <td>Nama</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="family in families.data" :key="family.id">
+                  <td class="align-top">{{ family.head_of_family }}</td>
+                  <td class="align-top">{{ family.address }}</td>
+                  <td class="align-top">{{ family.phone }}</td>
+                  <td class="align-top">
+                    <ul>
+                      <li v-for="muzakki in family.muzakkis" :key="muzakki.id">
+                        {{ muzakki.name }}
+                      </li>
+                    </ul>
+                  </td>
+                  <td class="align-top">{{ family.user?.email }}</td>
+                  <td class="align-top">{{ family.user?.name }}</td>
+                </tr>
+              </tbody>
+            </table>
+            <pagination :links="families.links" />
+          </div>
+        </div>
+      </div>
+    </div>
   </BreezeAuthenticatedLayout>
 </template>
 <script>
