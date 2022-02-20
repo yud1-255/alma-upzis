119513713f57ec500641df3cc92d436c83c17568
yud1 <yud1@csui04.net>
2022-02-20 18:35:07 +0700

    implement custom reset password notification email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\MailResetPasswordToken;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -68,5 +69,10 @@ class User extends Authenticatable
         }
 
         return false;
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new MailResetPasswordToken($token, $this->email));
     }
 }
---
+++ app/Notifications/MailResetPasswordToken.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class MailResetPasswordToken extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token, $email)
+    {
+        $this->token = $token;
+        $this->email = $email;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Permintaan untuk reset password anda')
+            ->line('Assalamualaikum,')
+            ->line('Anda menerima email ini sehubungan dengan permintaan untuk reset password untuk akun anda.')
+            ->action('Reset Password', route('password.reset', [$this->token, 'email' => $this->email]))
+            ->line('Silakan menghubungi administrator UPZIS apabila anda memiliki pertanyaan lebih lanjut.')
+            ->line('Wassalamualaikum warahmatullahi wabarakatuh');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
