6437e1b3c77f84cf742b39dc9a4621947e298b75
yud1 <yud1@csui04.net>
2022-03-09 23:20:48 +0700

    refine table styling

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="min-h-screen bg-gray-100">
-      <nav class="bg-white border-b border-yellow-200">
+      <nav class="bg-white border-b-2 border-yellow-200">
         <!-- Primary Navigation Menu -->
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 print:hidden">
           <div class="flex justify-between h-16">
