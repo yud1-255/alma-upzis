18297baaea5e96f8c60f58490fcb48e2a469f745
yud1 <yud1@csui04.net>
2022-03-12 20:23:20 +0700

    implement responsive element for zakat show

--- resources/js/Pages/Zakat/Show.vue
+++ resources/js/Pages/Zakat/Show.vue
@@ -12,20 +12,96 @@
       <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
           <div class="p-6 bg-white border-b border-gray-200 print:font-mono">
-            <ZakatReceipt :zakat="zakat"></ZakatReceipt>
-
-            <div v-if="printWithCopy" class="hidden print:block">
-              <div class="py-4 text-center">
-                sisi untuk muzakki
-                <hr />
-                sisi untuk petugas
-              </div>
-
+            <div class="hidden md:block print:block">
               <ZakatReceipt :zakat="zakat"></ZakatReceipt>
+
+              <div v-if="printWithCopy" class="hidden print:block">
+                <div class="py-4 text-center">
+                  sisi untuk muzakki
+                  <hr />
+                  sisi untuk petugas
+                </div>
+
+                <ZakatReceipt :zakat="zakat"></ZakatReceipt>
+              </div>
+            </div>
+
+            <div class="block md:hidden print:hidden">
+              <div class="py-4 font-semibold text-left">
+                <h2>Transaksi no: {{ zakat.transaction_no }}</h2>
+              </div>
+              <table class="table-auto w-full">
+                <thead>
+                  <th>Muzakki</th>
+                  <th>Kg</th>
+                  <th>Lt</th>
+                  <th>Rp</th>
+                </thead>
+
+                <tbody>
+                  <tr
+                    v-for="zakat_line in zakat.zakat_lines"
+                    :key="zakat_line.id"
+                  >
+                    <td>{{ zakat_line.muzakki.name }}</td>
+                    <td class="text-right">
+                      {{
+                        (
+                          Number(zakat_line.fitrah_kg) +
+                          Number(zakat_line.fidyah_kg)
+                        ).toLocaleString("id")
+                      }}
+                    </td>
+                    <td class="text-right">
+                      {{ Number(zakat_line.fitrah_lt).toLocaleString("id") }}
+                    </td>
+                    <td class="text-right">
+                      {{
+                        (
+                          Number(zakat_line.fitrah_rp) +
+                          Number(zakat_line.maal_rp) +
+                          Number(zakat_line.profesi_rp) +
+                          Number(zakat_line.infaq_rp) +
+                          Number(zakat_line.fidyah_rp) +
+                          Number(zakat_line.wakaf_rp) +
+                          Number(zakat_line.kafarat_rp)
+                        ).toLocaleString("id")
+                      }}
+                    </td>
+                  </tr>
+                </tbody>
+                <tfoot class="border-t border-gray-200">
+                  <tr v-if="!zakat.is_offline_submission">
+                    <td class="font-semibold">Biaya Unik</td>
+                    <td></td>
+                    <td></td>
+                    <td class="text-right">
+                      {{ Number(zakat.unique_number).toLocaleString("id") }}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="font-semibold">Total (Rp)</td>
+                    <td class="text-right">-</td>
+                    <td class="text-right">-</td>
+                    <td class="text-right">
+                      {{ Number(zakat.total_transfer_rp).toLocaleString("id") }}
+                    </td>
+                  </tr>
+                </tfoot>
+              </table>
+              <div class="mt-6 p-2 text-left grid grid-rows-4 grid-cols-2">
+                <div>Tanggal:</div>
+                <div class="text-right">{{ zakat.transaction_date }}</div>
+
+                <div>Terima dari:</div>
+                <div class="text-right">{{ zakat.receive_from_name }}</div>
+                <div>Petugas:</div>
+                <div class="text-right">{{ zakat.zakat_pic?.name }}</div>
+              </div>
             </div>
 
             <!-- <div v-if="!(can.print && can.confirmPayment)"> -->
-            <div class="print:hidden">
+            <div v-if="zakat.zakat_pic == null" class="print:hidden">
               <div class="my-4">
                 <p class="text-sm font-semibold my-2">Catatan untuk muzakki:</p>
                 <ol class="list-decimal list-inside text-sm">
@@ -91,12 +167,20 @@
                 </popup-modal>
               </div>
             </div>
+            <div v-else class="mb-4 print:hidden">
+              <p class="text-sm font-semibold my-2">Catatan untuk muzakki:</p>
+
+              <p class="text-sm">
+                Pembayaran telah dikonfirmasi untuk transaksi ini. Silakan
+                menghubungi panitia untuk mendapatkan tanda terima.
+              </p>
+            </div>
             <div
               v-if="can.print || can.confirmPayment"
               class="pt-6 space-y-1 md:space-x-2 print:hidden"
             >
               <Button
-                v-if="can.confirmPayment"
+                v-if="can.confirmPayment && zakat.zakat_pic == null"
                 class="w-full md:w-auto"
                 @click="confirmPayment(zakat.id)"
               >
